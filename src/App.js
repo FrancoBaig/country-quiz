9e1c2c6f78f0e2bc0b882676d41b23f98b31baec
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-05-24 17:48:29 -0300

    sass config

--- src/App.js
+++ src/App.js
@@ -12,6 +12,41 @@ function App() {
                     src="../public/images/adventure.svg"
                     alt="adventure"
                 />
+                <div className="card-body">
+                    <h3 className="card-quiestion">
+                        Kuala Lumpur is the capital of
+                    </h3>
+                    <div className="answers-container">
+                        <div className="answer-item">
+                            <p>A</p>
+                            <p>Vietnam</p>
+                            <div className="result-icon">
+                                <i class="fa-regular fa-circle-check"></i>
+                            </div>
+                        </div>
+                        <div className="answer-item">
+                            <p>A</p>
+                            <p>Vietnam</p>
+                            <div className="result-icon">
+                                <i class="fa-regular fa-circle-check"></i>
+                            </div>
+                        </div>
+                        <div className="answer-item">
+                            <p>A</p>
+                            <p>Vietnam</p>
+                            <div className="result-icon">
+                                <i class="fa-regular fa-circle-check"></i>
+                            </div>
+                        </div>
+                        <div className="answer-item">
+                            <p>A</p>
+                            <p>Vietnam</p>
+                            <div className="result-icon">
+                                <i class="fa-regular fa-circle-check"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
