18adf1865835da55ec26c19fa6f9ad0c910c6b28
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-05-25 17:41:29 -0300

    send questions fixed

--- src/App.js
+++ src/App.js
@@ -1,51 +1,46 @@
 import "./App.scss";
 
 import React, { useEffect, useState } from "react";
-import useSound from "use-sound";
-import correct from "./Assets/sounds/sound_correct.wav";
-import wrong from "./Assets/sounds/sound_wrong.wav";
+
 import Card from "./components/Card";
 import useCountry from "./hooks/useCountry";
 import Loading from "./components/Loading";
 
+const data = {
+    title: "Malabo is the capital of",
+    country: "Equatorial Guinea",
+    options: [
+        {
+            id: "A",
+            text: "Isle of Man",
+        },
+        {
+            id: "B",
+            text: "Equatorial Guinea",
+        },
+        {
+            id: "C",
+            text: "El Salvador",
+        },
+        {
+            id: "D",
+            text: "British Indian Ocean Territory",
+        },
+    ],
+};
+const loading = false;
+
 function App() {
-    const { data, loading, error, submitRequest } = useCountry();
-    const [displayBtn, setDisplayBtn] = useState(false);
+    // const { error, submitRequest } = useCountry();
+    // const [displayBtn, setDisplayBtn] = useState(false);
     const [answer, setAnswer] = useState(null);
-    const [finishedTurn, setFinishedTurn] = useState(false);
+
     const [score, setScore] = useState(0);
-
-    const [playSuccess] = useSound(correct, { volume: 0.5 });
-    const [playWrong] = useSound(wrong, { volume: 0.2 });
-
-    const handleAnswerClick = (id) => {
-        if (finishedTurn) return;
-        setAnswer(id);
-
-        if (displayBtn) return;
-        setDisplayBtn(true);
-    };
+    const [selected, setSelected] = useState("");
 
     useEffect(() => {
-        submitRequest();
+        // submitRequest();
     }, []);
-
-    const handleSubmit = () => {
-        const selected = document.getElementsByClassName("selected")[0];
-        const correctAnswer = data.country;
-
-        if (selected.id === correctAnswer) {
-            selected.classList.add("success");
-            setScore(score + 1);
-            playSuccess();
-        } else {
-            selected.classList.add("warning");
-            let correct = document.getElementById(correctAnswer);
-            correct.classList.add("success");
-            playWrong();
-        }
-        setFinishedTurn(true);
-    };
 
     return (
         <div className="app">
@@ -55,13 +50,7 @@ function App() {
                 {!loading && data && (
                     <>
                         <h1 className="header">Country quiz</h1>
-                        <Card
-                            data={data}
-                            answer={answer}
-                            handleAnswerClick={handleAnswerClick}
-                            displayBtn={displayBtn}
-                            handleSubmit={handleSubmit}
-                        />
+                        <Card data={data} answer={answer} selected={selected} />
                     </>
                 )}
             </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import AdventureImage from "../Assets/images/adventure.svg";
+import useSound from "use-sound";
+import correct from "../Assets/sounds/sound_correct.wav";
+import wrong from "../Assets/sounds/sound_wrong.wav";
 
-function Card({ data, answer, handleAnswerClick, displayBtn, handleSubmit }) {
+function Card({ data, displayBtn }) {
+    const [finishedTurn, setFinishedTurn] = useState(false);
+    const [playSuccess] = useSound(correct, { volume: 0.5 });
+    const [playWrong] = useSound(wrong, { volume: 0.2 });
+    const handleAnswerClick = (id) => {
+        if (finishedTurn) return;
+        const selected = document.getElementById(id);
+        const correctAnswer = data.country;
+        if (selected.id === correctAnswer) {
+            selected.classList.add("success");
+            // setScore(score + 1);
+            playSuccess();
+        } else {
+            selected.classList.add("warning");
+            let correct = document.getElementById(correctAnswer);
+            correct.classList.add("success");
+            playWrong();
+        }
+        setFinishedTurn(true);
+    };
+
     return (
         <div className="card">
             <img className="card-img" src={AdventureImage} alt="adventure" />
@@ -12,26 +35,19 @@ function Card({ data, answer, handleAnswerClick, displayBtn, handleSubmit }) {
                         <div
                             key={elem.id}
                             id={elem.text}
-                            className={`answer-item ${
-                                answer === elem.id ? "selected" : ""
-                            }`}
-                            onClick={() => handleAnswerClick(elem.id)}
+                            className={`answer-item `}
+                            onClick={() => handleAnswerClick(elem.text)}
                         >
-                            <p>{elem.id}</p>
+                            {elem.id}
                             <p className="answer-text">{elem.text}</p>
-                            <div className="result-icon">
-                                <i className="fa-regular fa-circle-check"></i>
-                            </div>
+
+                            <i className="fa-regular fa-circle-check"></i>
                         </div>
                     ))}
                 </div>
 
-                {displayBtn && (
-                    <button
-                        type="button"
-                        className="btn-next"
-                        onClick={handleSubmit}
-                    >
+                {finishedTurn && (
+                    <button type="button" className="btn-next">
                         Next
                     </button>
                 )}
